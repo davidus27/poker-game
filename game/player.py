2f9670e3dfb97e432e36ef24a3cf5b228cb98c3b
dave <dave@null>
2019-07-27 16:51:15 +0200

    Creating player object

--- game/player.py
+++ game/player.py
@@ -4,3 +4,14 @@ Author:dave
 Github:https://github.com/davidus27
 Description:Player and variations of bots.
 """
+from random import random
+from printouts import options
+
+class Player(object):
+    def __init__(self, name = "Player", money = 500.0):
+        self.name = name
+        self.money = money
+        self.diff = 0.0
+        self.hand = []
+
+
