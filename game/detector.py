2cbd33dd8e780ff68835199509e0645dc13ddc3c
dave <dave@null>
2019-07-27 17:40:17 +0200

    creating object for better detection of poker hands-detector

--- game/detector.py
+++ game/detector.py
@@ -4,4 +4,11 @@ Author: dave
 Github: https://github.com/davidus27
 Description: Class for easier detection of all existing hand values of players.
 """
+class Detector(object):
+     
+    suits = {"Spades" : "♠" , "Clubs" : "♣" , "Diamonds" : "♦" , "Hearts": "♥"}
+    #numericalValues = {"Jack":11, "Queen":12, "King":13, "Ace":14}
+    cardsOrder = [2,3,4,5,6,7,8,9,10, "Jack", "Queen" , "King", "Ace"]
 
+    def __init__(self):
+        pass
