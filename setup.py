c64696b5077df945ebd6ea506c67dcadccfd7b4b
davidus27 <davidisbest27@gmail.com>
2025-05-16 21:38:21 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -9,20 +9,20 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "PokerGame",
+    name = "poker-game",
     version = "0.0.4",
     author = "Dave",
-    author_email = "l33tsux42@gmail.com",
-    description = ("Small summer project to escape from the boredom"
-                                   "just about it"),
+    author_email = "davidus27@gmail.com",
+    description = ("Small summer project to escape from the boredom"),
     license = "GNU",
-    keywords = "example documentation tutorial",
+    keywords = "poker game",
     url = "http:/github.com/davidus27/pokerGame",
-    packages=['an_example_pypi_project', 'tests'],
-    long_description=read('README'),
+    packages=find_packages(),
+    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
+        "Topic :: Games/Entertainment",
         "License :: OSI Approved :: GNU License",
     ],
+    python_requires='>=3.6',
 )
